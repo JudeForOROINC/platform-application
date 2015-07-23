6a0e3ef2da642f1ba475c42193c88b079bd1ce92
JudeForOROINC <gkudryashov@magecore.com>
2015-07-23 14:15:16 +0300

    OOT-821 : tt2: CRUD  - crud actions add

--- src/Magecore/Bundle/TestTaskOroBundle/Controller/IssueController.php
+++ src/Magecore/Bundle/TestTaskOroBundle/Controller/IssueController.php
@@ -10,6 +10,7 @@ namespace Magecore\Bundle\TestTaskOroBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -32,5 +33,61 @@ class IssueController extends Controller
     public function indexAction(){
         return array('entity_class'=>'MagecoreTestTaskOroBundle\Entity\Issue');
     }
+    /**
+     * @Route("/create", name="inventory.vehicle_create")
+     * @Template("InventoryBundle:Vehicle:update.html.twig")
+     * @Acl(
+     *     id="inventory.vehicle_create",
+     *     type="entity",
+     *     class="InventoryBundle:Vehicle",
+     *     permission="CREATE"
+     * )
+     */
+    public function createAction(Request $request)
+    {
+        return $this->update(new Vehicle(), $request);
+    }
+
+    /**
+     * @Route("/update/{id}", name="inventory.vehicle_update", requirements={"id":"\d+"}, defaults={"id":0})
+     * @Template()
+     * @Acl(
+     *     id="inventory.vehicle_update",
+     *     type="entity",
+     *     class="InventoryBundle:Vehicle",
+     *     permission="EDIT"
+     * )
+     */
+    public function updateAction(Vehicle $vehicle, Request $request)
+    {
+        return $this->update($vehicle, $request);
+    }
+
+    private function update(Vehicle $vehicle, Request $request)
+    {
+        $form = $this->get('form.factory')->create('inventory_vehicle', $vehicle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($vehicle);
+            $entityManager->flush();
+
+            return $this->get('oro_ui.router')->redirectAfterSave(
+                array(
+                    'route' => 'inventory.vehicle_update',
+                    'parameters' => array('id' => $vehicle->getId()),
+                ),
+                array('route' => 'inventory.vehicle_index'),
+                $vehicle
+            );
+        }
+
+        return array(
+            'entity' => $vehicle,
+            'form' => $form->createView(),
+        );
+    }
+
 
 }
---
+++ src/Magecore/Bundle/TestTaskOroBundle/Form/Type/IssueType.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jude
+ * Date: 23.07.15
+ * Time: 14:10
+ */
+
+
+namespace Magecore\Bundle\TestTaskOroBundle\Form\Type;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\DependencyInjection\Security\UserProvider\EntityFactory;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Magecore\Bundle\TestTaskOroBundle\Entity\Issue;
+
+class IssueType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('summary', null, array('label'=>'field.summary.issue'))
+            ->add('description', null, array('label'=>'field.description'))
+            ->add('assignee', null, array('label'=>'field.assigned'))
+        ;
+
+//        if (isset($options['projects'])&& count($options['projects'])) {
+//            $pro = array();
+//            if (count($options['projects'])==1) {
+//                $options['data']->setProject($options['projects'][0]);
+//            } else {
+//                foreach ($options['projects'] as $project) {
+//                    $pro[$project->getId()] = (string)$project;
+//                }
+//                $builder->add(
+//                    'project',
+//                    'entity',
+//                    array(
+//                        'choices' => $options['projects'],
+//                        'label' => 'field.project',
+//                        'class' => 'Magecore\Bundle\TestTaskBundle\Entity\Project'
+//                    )
+//                );
+//            }
+//
+//        }
+
+//        if (isset($options['data'])) {
+//            if (get_class($options['data']) == 'Magecore\Bundle\TestTaskBundle\Entity\Issue') {
+//                if ($options['data']->getId() == 0) {
+//                    if (!empty($options['data']->getParentIssue())) {
+//                        //do nothing.
+//                        $arr = null;
+//                    } else {
+//                        $arr = $options['data']->getParentTypes();
+//                        $arr = array_combine($arr, $arr);
+//                    }
+//                }
+//            }
+//
+//        }
+
+        if (!empty($arr)) {
+            $builder->add('type', 'choice', array(
+                'choices' =>
+                    $arr,
+
+                'required' => true,
+                'label'=>'field.type',
+                'empty_value' => false,
+                'empty_data'=>null,
+            ));
+        }
+//        $builder
+//            ->add('priority', 'entity', array('label'=>'field.priority',
+//                'class' => 'Magecore\Bundle\TestTaskBundle\Entity\DicPriority',
+//                'query_builder' => function (EntityRepository $er) {
+//                    return $er->createQueryBuilder('p')->orderBy('p.sortOrder', 'ASC');
+//                }
+//            ))
+//            ->add('status', null, array('label'=>'field.status'))
+//            ->add('resolution', null, array('label'=>'field.resolution'))
+//        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Magecore\Bundle\TestTaskOroBundle\Entity\Issue',
+            'projects' => array(),
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'magecore_bundle_testtaskbundle_issue';
+    }
+}
