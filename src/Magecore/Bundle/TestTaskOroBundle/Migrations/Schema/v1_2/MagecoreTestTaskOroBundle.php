c8d7b7f82d2003aee1c536c412d89523984bbbb5
JudeForOROINC <gkudryashov@magecore.com>
2015-07-29 13:48:06 +0300

    OOT-824 : tt5: notes + email  - email install

---
+++ src/Magecore/Bundle/TestTaskOroBundle/Migrations/Schema/v1_2/MagecoreTestTaskOroBundle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Magecore\Bundle\TestTaskOroBundle\Migrations\Schema\v1_2;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+
+class MagecoreTestTaskOroBundle implements Migration, ActivityExtensionAwareInterface
+{
+    /** @var ActivityExtension */
+    protected $activityExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->activityExtension->addActivityAssociation($schema, 'oro_email', 'magecore_testtaskoro_issue');
+    }
+}
